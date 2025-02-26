860c2f9353b149423fc5be5f33682d81b0310804
Jon Staab <shtaab@gmail.com>
2025-02-26 08:48:26 -0800

    Inline handlers

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
-import express, { Request } from 'express'
+import express, { Request, Response } from 'express'
 import addWebsockets, { Application } from 'express-ws'
 import rateLimit from 'express-rate-limit'
 import { WebSocket } from 'ws'
+import { appSigner } from './env.js'
+import { render } from './templates.js'
 import { Connection } from './relay.js'
-import { handleEmailConfirm, handleEmailRemove, handleNip11, handleConfirm, handleUnsubscribe } from './handlers.js'
+import { confirmEmailUser, authenticateEmailUser, removeEmailUser } from './database.js'
+
+// Utils
+
+const _err = (res: Response, status: number, error: string) => {
+  res.status(status).send({error})
+}
+
+const _ok = (res: Response, status = 200) => {
+  res.status(status).send({ok: true})
+}
+
+// Endpoints
 
 export const server: Application = express() as unknown as Application
 
@@ -12,11 +26,50 @@ addWebsockets(server)
 server.use(rateLimit({limit: 30, windowMs: 5 * 60 * 1000}))
 server.use(express.json())
 
-server.get('/', handleNip11)
-server.get('/confirm', handleConfirm)
-server.get('/unsubscribe', handleUnsubscribe)
-server.post('/email/confirm', handleEmailConfirm)
-server.post('/email/unsubscribe', handleEmailRemove)
+server.get('/', async (_req: Request, res: Response) => {
+  res.set({'Content-Type': 'application/nostr+json; charset=utf-8'})
+
+  res.json({
+    name: "Anchor",
+    icon: "https://pfp.nostr.build/2644089e06a950889fa4aa81f6152a51fba23497735cbba351aa6972460df6f5.jpg",
+    description: "A relay/notifier combo for email notifications",
+    pubkey: await appSigner.getPubkey(),
+    software: "https://github.com/coracle-social/anchor",
+  })
+})
+
+server.get('/confirm', async (req: Request, res: Response) => {
+  res.send(await render('pages/confirm.html', req.query))
+})
+
+server.get('/unsubscribe', async (req: Request, res: Response) => {
+  res.send(await render('pages/unsubscribe.html', req.query))
+})
+
+server.post('/email/confirm', async (req: Request, res: Response) => {
+  const {email, confirm_token} = req.body
+
+  const confirmed = await confirmEmailUser({email, confirm_token})
+
+  if (confirmed) {
+    _ok(res)
+  } else {
+    _err(res, 400, "It looks like that confirmation code is invalid or has expired.")
+  }
+})
+
+server.post('/email/unsubscribe', async (req: Request, res: Response) => {
+  const {email, access_token} = req.body
+
+  const authenticated = await authenticateEmailUser({email, access_token})
+
+  if (authenticated) {
+    await removeEmailUser({email})
+    _ok(res)
+  } else {
+    _err(res, 401, "Invalid access token")
+  }
+})
 
 server.ws('/', (socket: WebSocket, request: Request) => {
   const connection = new Connection(socket, request)
