75864ed77e791c26baea2f3a220eea94a79a4729
Jon Staab <shtaab@gmail.com>
2025-06-19 11:32:19 -0700

    Add vapid key

--- src/env.ts
+++ src/env.ts
@@ -9,6 +9,7 @@ if (!process.env.ANCHOR_NAME) throw new Error('ANCHOR_NAME is not defined.')
 if (!process.env.ANCHOR_SECRET) throw new Error('ANCHOR_SECRET is not defined.')
 if (!process.env.POSTMARK_API_KEY) throw new Error('POSTMARK_API_KEY is not defined.')
 if (!process.env.POSTMARK_SENDER_ADDRESS) throw new Error('POSTMARK_SENDER_ADDRESS is not defined.')
+if (!process.env.VAPID_PRIVATE_KEY) throw new Error('VAPID_PRIVATE_KEY is not defined.')
 if (!process.env.DEFAULT_RELAYS) throw new Error('DEFAULT_RELAYS is not defined.')
 if (!process.env.INDEXER_RELAYS) throw new Error('INDEXER_RELAYS is not defined.')
 if (!process.env.SEARCH_RELAYS) throw new Error('SEARCH_RELAYS is not defined.')
@@ -19,6 +20,7 @@ export const ANCHOR_NAME = process.env.ANCHOR_NAME
 export const appSigner = Nip01Signer.fromSecret(process.env.ANCHOR_SECRET)
 export const POSTMARK_API_KEY = process.env.POSTMARK_API_KEY
 export const POSTMARK_SENDER_ADDRESS = process.env.POSTMARK_SENDER_ADDRESS
+export const VAPID_PRIVATE_KEY = process.env.VAPID_PRIVATE_KEY
 export const DEFAULT_RELAYS = process.env.DEFAULT_RELAYS.split(',').map(normalizeRelayUrl)
 export const INDEXER_RELAYS = process.env.INDEXER_RELAYS.split(',').map(normalizeRelayUrl)
 export const SEARCH_RELAYS = process.env.SEARCH_RELAYS.split(',').map(normalizeRelayUrl)
